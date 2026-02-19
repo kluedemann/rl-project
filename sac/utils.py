f9d2ccdd23e3a3489128f471a3b683062a23a5bb
Kai Luedemann <kai.s.luedemann@gmail.com>
2026-02-19 17:54:31 +0100

    Documented utils.py

--- sac/utils.py
+++ sac/utils.py
@@ -5,6 +5,29 @@ from sac.memory import Memory
 from sac.SAC import *
 import pickle
 
+
+SCALING = np.asarray([ 1.0,  1.0 , 0.5, 4.0, 4.0, 4.0,  
+            1.0,  1.0,  0.5, 4.0, 4.0, 4.0,  
+            2.0, 2.0, 10.0, 10.0, 4.0 ,4.0])
+ACTION_BOUNDS = (-np.ones(4), np.ones(4))
+
+
+HOCKEY_PARAMS = {
+    # NOTE: Only hidden_sizes is relevant if loading agent
+    "lr_actor": 3e-4,
+    "lr_critic": 3e-4,
+    "tau": 1-0.005,
+    "batch_size": 256,
+    "gamma": 0.99,
+    "alpha": 0.01,
+    "loss": "MSE",
+    "hidden_sizes": [256, 256],
+    "obs_dim": len(SCALING),
+    "obs_scale": SCALING,
+    "action_bounds": ACTION_BOUNDS
+}
+
+# Source: https://stable-baselines3.readthedocs.io/en/master/modules/sac.html#parameters
 SB3_PARAMS = {
     # "fit_steps": 1,
     "lr_actor": 3e-4,
@@ -17,6 +40,7 @@ SB3_PARAMS = {
     # "exp_steps": 100
 }
 
+# Source: https://spinningup.openai.com/en/latest/algorithms/sac.html#documentation
 SPINNING_UP_PARAMS = {
     # "fit_steps": 1,
     "lr_actor": 1e-3,
@@ -34,50 +58,101 @@ LOSSES = {
 }
 
 
-def from_dict(env, hidden_sizes, lr_critic, lr_actor, loss, tau, alpha, gamma, batch_size, action_bounds, obs_scale=1.):
-    loss_f = LOSSES[loss]
+def get_trained_agent(filepath, params=HOCKEY_PARAMS):
+    """Create a trained agent from a state file.
     
-    observation_dim = env.observation_space.shape[0]
+    Params:
+        filepath - path to saved model state; *.pth
+        params - agent parameters
+    """
+    new_agent = from_dict(**params)
+    state = torch.load(filepath)
+    new_agent.restore_state(state)
+    return new_agent
+
+
+def from_dict(hidden_sizes, lr_critic, lr_actor, loss, tau, alpha, gamma, batch_size, action_bounds, obs_dim, obs_scale=1., buffer_size=int(1e6)):
+    """Create new SAC agent.
+    
+    Params:
+        hidden_sizes - hidden layer widths for value function and policy networks
+        lr_critic - critic learning rate
+        lr_actor - actor learning rate
+        loss - critic loss function; accepts (values, targets)
+        tau - Polyak update coefficient
+        alpha - entropy regularization coefficient
+        gamma - discount factor
+        batch_size - update batch size
+        action_bounds - (low, high) the bounds for the action space of shape (action_dim,)
+        obs_dim - dimension of observation space
+        obs_scale - scaling factor to divide observations by; float or np.array (state_dim,)
+        buffer_size - maximum size of replay buffer
+    
+    Returns: the initialized SAC agent
+    """
+    loss_f = LOSSES[loss]
+    observation_dim = obs_dim
     action_dim = action_bounds[0].shape[0]
     
+    # Initialize first Q function
     Q1_base = Feedforward(observation_dim+action_dim, hidden_sizes, 1)
     Q1_target = Feedforward(observation_dim+action_dim, hidden_sizes, 1)
     Q1_optim = torch.optim.Adam(Q1_base.parameters(),
                                             lr=lr_critic)
     Q1 = QFunction(Q1_base, Q1_target, Q1_optim, loss_f, tau)
     
+    # Initialize second Q function
     Q2_base = Feedforward(observation_dim+action_dim, hidden_sizes, 1)
     Q2_target = Feedforward(observation_dim+action_dim, hidden_sizes, 1)
     Q2_optim = torch.optim.Adam(Q2_base.parameters(), lr=lr_critic)
     Q2 = QFunction(Q2_base, Q2_target, Q2_optim, loss_f, tau)
     
+    # Initialize policy
     policy_base = Feedforward(observation_dim, hidden_sizes, 2*action_dim)
     policy_optim = torch.optim.Adam(policy_base.parameters(), lr=lr_actor)
     policy = TanhGaussianPolicy(policy_base, policy_optim, action_bounds)
-    buffer = Memory(int(1e6))
+    
+    # Create SAC agent
+    buffer = Memory(buffer_size)
     sac = SAC(Q1, Q2, policy, alpha=alpha, gamma=gamma, buffer=buffer, batch_size=batch_size, obs_scale=obs_scale)
     return sac
 
 
-def save_statistics(filepath, rewards, lengths, losses):
-    with open(f"{filepath}-stat.pkl", 'wb') as f:
-        pickle.dump({"rewards" : rewards, "lengths": lengths, "losses": losses}, f)
-
-def load_stats(filepath):
-    with open(f"{filepath}-stat.pkl", 'rb') as f:
-        data = pickle.load(f)
-    return data["rewards"], data["lengths"], data["losses"]
-
 def save_logs(filepath, logs):
+    """Save training logs.
+    
+    Params:
+        filepath - the base path to file; extension will be added
+        logs - (dict) the logs to save
+    """
     with open(f"{filepath}-stat.pkl", 'wb') as f:
         pickle.dump(logs, f)
 
+
 def load_logs(filepath):
+    """Load training logs.
+    
+    Params:
+        filepath - the base path to file; extension will be added
+    
+    Returns: (dict) - the training logs
+    """
     with open(f"{filepath}-stat.pkl", 'rb') as f:
         data = pickle.load(f)
     return data
 
 
+# ********************** OLD INTERFACE ****************************************
+
+def save_statistics(filepath, rewards, lengths, losses):
+    with open(f"{filepath}-stat.pkl", 'wb') as f:
+        pickle.dump({"rewards" : rewards, "lengths": lengths, "losses": losses}, f)
+
+
+def load_stats(filepath):
+    with open(f"{filepath}-stat.pkl", 'rb') as f:
+        data = pickle.load(f)
+    return data["rewards"], data["lengths"], data["losses"]
 
 
 def warmup_agent(agent, env, n_steps, max_timesteps):
