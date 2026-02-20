68ffa5fc706500c64baedae463bf79702db2de1d
Kai Luedemann <kai.s.luedemann@gmail.com>
2026-02-20 14:04:04 +0100

    Added competition server client

---
+++ sac/client.py
@@ -0,0 +1,34 @@
+from comprl.client import Agent
+import numpy as np
+import uuid
+
+
+class SACAgent(Agent):
+    """A hockey agent that can be weak or strong."""
+
+    def __init__(self, agent) -> None:
+        super().__init__()
+
+        self.agent = agent
+
+    def get_step(self, observation: list[float]) -> list[float]:
+        # NOTE: If your agent is using discrete actions (0-7), you can use
+        # HockeyEnv.discrete_to_continous_action to convert the action:
+        #
+        # from hockey.hockey_env import HockeyEnv
+        # env = HockeyEnv()
+        # continuous_action = env.discrete_to_continous_action(discrete_action)
+
+        action = self.agent.act(np.asarray(observation)).tolist()
+        return action
+
+    def on_start_game(self, game_id) -> None:
+        game_id = uuid.UUID(int=int.from_bytes(game_id))
+        print(f"Game started (id: {game_id})")
+
+    def on_end_game(self, result: bool, stats: list[float]) -> None:
+        text_result = "won" if result else "lost"
+        print(
+            f"Game ended: {text_result} with my score: "
+            f"{stats[0]} against the opponent with score: {stats[1]}"
+        )
